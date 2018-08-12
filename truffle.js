bdd20433a7be6c1906726d41319a5ef35803becc
mikiquantum <miguel.hervas.lazaro@gmail.com>
2018-08-12 19:41:18 +0200

    gitlab-ci

--- truffle.js
+++ truffle.js
@@ -1,19 +1,19 @@
 let WalletProvider = require("truffle-wallet-provider");
-// require('dotenv').config();
 
-// Read and unlock keystore
-let keystore = require('fs').readFileSync('./account').toString();
-let pass = "";
-let wallet = require('ethereumjs-wallet').fromV3(keystore, pass);
 let endpoint = process.env.PROVIDER_ENDPOINT || "http://127.0.0.1:8545";
-// console.log(wallet.getPrivateKey().toString('hex'));
+let b64account = process.env.ETH_ACCOUNT || "";
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
-    test: {
-      provider: new WalletProvider(wallet, endpoint),
+    dev: {
+      provider: () => {
+        let keystore = Buffer.from(b64account, 'base64').toString();
+        let pass = "";
+        let wallet = require('ethereumjs-wallet').fromV3(keystore, pass);
+        return new WalletProvider(wallet, endpoint)
+      },
       network_id: "17"
     },
     ganache: {
